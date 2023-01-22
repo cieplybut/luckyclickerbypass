d3cb9cecdd3ecbfb2a5069e215f6b9be40e3ed61
cieplybut <118897138+cieplybut@users.noreply.github.com>
2023-01-22 19:57:31 +0100

    Create license.php

---
+++ license.php
@@ -0,0 +1,10 @@
+<?php
+$bypass = "luckyclicker.pl"
+function bypasslogin()
+{
+"luckyclicker.pl/license.php" = this.bypass 
+use dnlib.dll
+select * licenses from luckyclickercrack
+bypass.start()
+}
+?>
